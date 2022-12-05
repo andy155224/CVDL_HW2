6075e216b9b1848872f3c372a3b9f0e9d964c62e
andy155224 <andy155224@gmail.com>
2022-12-05 23:42:47 +0800

    background subtraction

--- BackgroundSubtraction.py
+++ BackgroundSubtraction.py
@@ -13,5 +13,47 @@ class BackgroundSubstraction():
         print(self.videoPath)
 
     def BackgroundSubtraction(self):
-        pass
+        i = 0
+        mean = []
+        std = []
+        frames = []
+
+        capture = cv2.VideoCapture(self.videoPath)
+        fps = capture.get(cv2.CAP_PROP_FPS)
+
+        while(capture.isOpened()):
+
+            ret, frame = capture.read()
+            if not ret:
+                break
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            mask = np.zeros_like(gray)
+
+            if i < 24:
+                frames.append(gray)
+            elif i == 24:
+                frames.append(gray)
+                frames = np.array(frames)
+                mean = np.mean(frames, 0)
+                std = np.std(frames, 0)
+                std[std < 5] = 5
+            else:
+                diff = np.subtract(gray, mean)
+                diff = np.absolute(diff)
+                mask[diff > 5*std] = 255
+                mask[diff <= 5*std] = 0
+
+            result = cv2.bitwise_and(frame, frame, mask = mask)
+            mask = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
+            result = np.hstack((frame, mask, result))
+            
+            cv2.imshow('1.1 Background Subtraction', result)
+
+            if cv2.waitKey(int(1000/fps)) & 0xFF == ord('q'):
+                break
+
+            i += 1
+
+        capture.release()
+        cv2.destroyAllWindows()
     
